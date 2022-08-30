88ce902749f635c572f82b4756f055e842532859
Rahul Kumar <rahulloni000@gmail.com>
2022-08-30 23:58:14 +0545

    react First Commit

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+import './App.css'
+import SubComponents from "./components/SubComponents";
+
+function App(){
+    return (
+    <div  className="app-container" >
+        <h1>first Components </h1>
+        <p>first sss </p>
+        <SubComponents/>
+
+    </div>
+    );
+
+}
+export default App;
---
+++ my-app/src/components/SubComponents.jsx
@@ -0,0 +1,11 @@
+ import './SubComponents.css'
+ function SubComponents(){
+    return(
+     <div>
+        <h1 className="red">
+            Sub Components
+        </h1>
+     </div>  
+    );
+ }
+ export default SubComponents;
