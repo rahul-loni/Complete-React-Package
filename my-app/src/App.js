d33214b2622a0722bdd7b4b1a8883081827c51d6
Rahul Kumar <rahulloni000@gmail.com>
2022-08-31 01:18:01 +0545

    single components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,62 @@
 
 import React from "react";
-import './App.css'
-import SubComponents from "./components/SubComponents";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App(){
-    return (
-    <div  className="app-container" >
-        <h1>first Components </h1>
-        <p>first sss </p>
-        <SubComponents/>
+    let expense=[
+        {
+            id:'e1',
+            title: 'School Fee',
+            date: new Date(2022, 2, 12),
+            amount:200
 
-    </div>
+        },
+        {
+            id:'e2',
+            title: 'Book Fee',
+            date: new Date(2022, 7, 12),
+            amount:300
+
+        },
+        {
+            id:'e3',
+            title: 'bus Fee',
+            date: new Date(2022, 9, 12),
+            amount:400
+
+        },
+        {
+            id:'e4',
+            title: 'house Fee',
+            date: new Date(2022, 22, 12),
+            amount:200
+
+        }
+    ]
+
+    return (
+        <div> 
+            <ExpenseItem 
+            date={expense[0].date} 
+            title={expense[0].title} 
+            amount={expense[0].amount}>
+            </ExpenseItem>
+            <ExpenseItem 
+            date={expense[1].date} 
+            title={expense[1].title} 
+            amount={expense[1].amount}>
+            </ExpenseItem>
+            <ExpenseItem 
+            date={expense[2].date} 
+            title={expense[2].title} 
+            amount={expense[2].amount}>
+            </ExpenseItem>
+            <ExpenseItem 
+            date={expense[3].date} 
+            title={expense[3].title} 
+            amount={expense[3].amount}>
+            </ExpenseItem>
+        </div>
     );
 
 }
